212bdb195cf7f61dcff2dcb82b0a0cd555dddbe4
Noor-devops <noorshanu07@gmail.com>
2026-09-08 13:06:47 +0530

    shell

--- expensev2.sh
+++ expensev2.sh
@@ -60,7 +60,7 @@ do
             --query 'Reservations[*].Instances[*].PrivateIpAddress' \
             --output text
             )
-            R53_RECORD="$instance.$DOMAIN_NAME"
+            #R53_RECORD="$instance.$DOMAIN_NAME"
         fi
         aws route53 change-resource-record-sets \
         --hosted-zone-id $ZONE_ID \
